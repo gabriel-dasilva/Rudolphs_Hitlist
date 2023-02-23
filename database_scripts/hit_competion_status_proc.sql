27819c7a925baeb1180694404c0d7efbaeb42aaa
devlon-erasmus-bbd <devlon@bbd.co.za>
2023-02-23 13:53:11 +0200

    update proc

--- database_scripts/hit_competion_status_proc.sql
+++ database_scripts/hit_competion_status_proc.sql
@@ -1,10 +1,9 @@
-CREATE PROCEDURE [dbo].[get_hit_completion_status]
-@grad_name varchar
-AS
-  SELECT h.hit_completion_status
-FROM hitlist AS h
-  INNER JOIN grad_information AS g
+CREATE OR REPLACE PROCEDURE get_hit_completion_status(grad_name varchar) 
+LANGUAGE sql
+AS $$
+	SELECT hit_completion_status
+	FROM hitlist AS h
+  	INNER JOIN grad_information AS g
     ON h.grad_id = g.grad_id
-WHERE g.name = @grad_name
-
-GO
+	WHERE g.name = grad_name
+$$;
