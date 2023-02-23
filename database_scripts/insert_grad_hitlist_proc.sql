9c43186f3b88b64ce05ae47e79e6a9808ddf3aa7
devlon-erasmus-bbd <122362441+devlon-erasmus-bbd@users.noreply.github.com>
2023-02-23 14:08:26 +0200

    Update

--- database_scripts/insert_grad_hitlist_proc.sql
+++ database_scripts/insert_grad_hitlist_proc.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE PROCEDURE insert_grad_into_hitlist(
 grad_id int,
 murder_method_id int,
 body_disposal_method_id int,
-hit_completion_status bit) 
+hit_completion_status bit = '0') 
 LANGUAGE sql
 AS $$
 	INSERT INTO hitlist (grad_id, murder_method_id,
